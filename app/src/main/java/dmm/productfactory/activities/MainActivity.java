52e604be2a53ce4fad71e61fdaaffb0f95be77a5
Dominika Maciejewicz <dominikamatyjewiczmaciejewicz@gmail.com>
2018-08-06 12:27:29 +0200

    Menu added

--- app/src/main/java/dmm/productfactory/activities/MainActivity.java
+++ app/src/main/java/dmm/productfactory/activities/MainActivity.java
@@ -1,8 +1,11 @@
 package dmm.productfactory.activities;
 
 import android.arch.persistence.room.Room;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -39,5 +42,23 @@ public class MainActivity extends AppCompatActivity {
 
     public String getGeneralText(){
         return generalTextView.getText().toString();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);    return true;}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return selectItem(item);}
+
+    public boolean selectItem(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_toc:
+                startActivity(new Intent(this,TocActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);    }
     }
 }
